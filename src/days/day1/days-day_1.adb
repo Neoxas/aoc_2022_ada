06d5ade1ab32a6517aef72a99f470e3562d3e5a5
Alex Powell <alex@volantautonomy.com>
2022-12-01 19:07:33 +0000

    Added comments

--- src/days/day1/days-day_1.adb
+++ src/days/day1/days-day_1.adb
@@ -6,6 +6,8 @@ package body Days.Day_1 with SPARK_Mode => Off is
       Ada.Containers.Vectors( Index_Type => Natural, Element_Type => Natural );
 
     function Get_Calories_From_File( Input_File: String ) return Nat_Vec.Vector is
+        -- Gets a Vector containing each elf with the relevant calories
+        -- Each Elf in the input file is seperated by a blank line
         File : File_Type;
         Vec : Nat_Vec.Vector;
         Curr_Elf : Natural := Natural'First;
@@ -24,6 +26,7 @@ package body Days.Day_1 with SPARK_Mode => Off is
                 end if;
             end;
         end loop;
+        -- Account for file not ending on a blank line
         Vec.Append( Curr_Elf );
 
         Close(File => File);
@@ -31,7 +34,8 @@ package body Days.Day_1 with SPARK_Mode => Off is
         return Vec;
     end Get_Calories_From_File;
 
-    function Get_Max_Idx_Of_Int_Vec( Vec: Nat_Vec.Vector ) return Natural is
+    function Get_Max_Idx( Vec: Nat_Vec.Vector ) return Natural is
+        -- Get index of maximum value from vector
         Tmp : Natural := Natural'First;
         Max_Idx : Natural := 0;
     begin
@@ -43,23 +47,25 @@ package body Days.Day_1 with SPARK_Mode => Off is
         end loop;
 
         return Max_Idx;
-    end Get_Max_Idx_Of_Int_Vec;
+    end Get_Max_Idx;
 
 
     procedure Run_Day_1_Part_1 ( Calories_Vec: Nat_Vec.Vector ) is
+        -- Run Part 1 to find the elf who is carrying the most calories
     begin
         Put_Line (Item => "Max elf calories: " & Natural'Image(
-                  Calories_Vec(Get_Max_Idx_Of_Int_Vec(Calories_Vec)
+                  Calories_Vec(Get_Max_Idx(Calories_Vec)
                    )));
     end Run_Day_1_Part_1;
 
     procedure Run_Day_1_Part_2 ( Calories_Vec: Nat_Vec.Vector; Elves_To_Count: Natural ) is
+        -- Run Part 2 to find the sum of calories for the top X elves
         Total_Calories: Natural := Natural'First;
         Cal_Copy : Nat_Vec.Vector := Calories_Vec;
         Idx: Natural;
     begin
         for I in Natural'First .. Elves_To_Count - 1 loop
-            Idx := Get_Max_Idx_Of_Int_Vec( Cal_Copy );
+            Idx := Get_Max_Idx( Cal_Copy );
             Total_Calories := Total_Calories + Cal_Copy( Idx );
             -- Remove entries from Calories
             Cal_Copy.Delete( Idx );
@@ -68,6 +74,7 @@ package body Days.Day_1 with SPARK_Mode => Off is
     end Run_Day_1_Part_2;
 
     procedure Run_Day_1 ( Input_File: String ) is
+        -- Get results for AOC Day 1 printed to the command line
         Cal_Vec : constant Nat_Vec.Vector := Get_Calories_From_File( Input_File );
     begin
         Put_Line( "--- Day 1 ---" );
