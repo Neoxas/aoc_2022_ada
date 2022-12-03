703974bf2b9106197d82919076798d9e3f173598
Alex <alexjohn_powell@hotmail.co.uk>
2022-12-03 10:46:48 +0000

    Made bounded string a subtype, not a new type

--- src/days/day3/days-day3.adb
+++
@@ -1,8 +0,0 @@
-package body Days.Day3 is
-
-   function Get_Value_Of_Duplicates( ) return Natural is
-   begin
-      return 0;
-   end Get_Value_Of_Duplicates;
-
-end Days.Day3;
--- src/days/day3/days-day3.ads
+++
@@ -1,5 +0,0 @@
-package Days.Day3 is
-
-   function Get_Value_Of_Duplicates( ) return Natural;
-
-end Days.Day3;
