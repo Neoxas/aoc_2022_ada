92fe3c6a716d4027f8c986bfcc67a2d4e3a73129
Alex <alexjohn_powell@hotmail.co.uk>
2022-12-04 11:37:50 +0000

    Solved Day 4 part 1

--- src/days/day4/days-day_4.adb
+++ src/days/day4/days-day_4.adb
@@ -1,7 +1,6 @@
-with Ada.Text_IO; use Ada.Text_IO;
-
 package body Days.Day_4 with SPARK_Mode is 
    subtype Clean_Str is Cleaning_Str_P.Bounded_String;
+   subtype Clean_Vec is Cleaning_Vec_P.Vector;
    
    
    procedure Split_String( Str: Clean_Str; Split: String; LHS_Str: out Clean_Str;  RHS_Str: out Clean_Str) is
@@ -29,14 +28,21 @@ package body Days.Day_4 with SPARK_Mode is
       Elf_2 := Convert_Str_To_Elf( Elf_2_Str);
    end Get_Elves;
    
-   function Count_Fully_Overlapping_Cleaning( Cleaning_Vec : Cleaning_Vec_P.Vector ) return Natural is
+   function Fully_Overlap( Elf_1 : Elf_R; Elf_2 : Elf_R ) return Boolean is 
+     (if ( ( Elf_2.Start <= Elf_1.Start and Elf_2.Finish >= Elf_1.Finish ) or
+           ( Elf_1.Start <= Elf_2.Start and Elf_1.Finish >= Elf_2.Finish ) )
+      then True else False);
+   
+   function Count_Fully_Overlapping_Cleaning( Cleaning_Vec : Clean_Vec ) return Natural is
       Elf_1: Elf_R;
       Elf_2: Elf_R;
       Result: Natural := 0;
    begin
       for Cleaning of Cleaning_Vec loop
          Get_Elves( Cleaning, Elf_1, Elf_2 );
-         Result := Result + Elf_2.Start + Elf_1.Start;
+         if Fully_Overlap( Elf_1, Elf_2 ) then
+            Result := Result + 1;
+         end if;
       end loop;
       return Result;
    end Count_Fully_Overlapping_Cleaning;
