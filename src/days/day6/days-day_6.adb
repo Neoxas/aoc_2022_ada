a6426ee3f389b33738bc1e31a13706675fb88320
Alex Powell <alex@volantautonomy.com>
2022-12-08 17:03:25 +0000

    Out by one error to solve

--- src/days/day6/days-day_6.adb
+++ src/days/day6/days-day_6.adb
@@ -12,6 +12,8 @@ package body Days.Day_6 is
       for Idx in 1 .. Window_Size_T(Length(String)) - Size loop
          Slice := Bounded_Slice(String, Idx, Idx + Size );
          if All_Entries_In_Slice_Unique( Slice ) then
+            -- There appears to be an out by one error here for some reason. 
+            -- This is only for message, not packet marker.
             First_Idx := Idx + Size - 1;
             exit;
          end if;
